5f1fc3d1bd659869d1ca73dab1e77fc2c6df9290
Ƶ𝔦x <158811830+zixja@users.noreply.github.com>
2026-03-30 01:24:53 -0400

    Update fxmanifest.lua

--- gang-raid/fxmanifest.lua
+++ gang-raid/fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 game 'gta5'
 lua54 'yes'
 
-description 'Gang Hideout Raid - Wave-based, Multi-location, QBCore/QBox/OX'
+description 'Gang Hideout Raid - QBCore/QBox/OX'
 author      'Zix'
 version     '1.0.0'
 
